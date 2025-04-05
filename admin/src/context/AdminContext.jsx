04e76b15e665b1eed24ef97d2f183638b5f46e74
Abhinav1730 <abhinav.saxena1730@gmail.com>
2025-04-05 23:03:41 +0530

    Creating Alll Doctor List Page in the Admin Panel and creating its context from backend.

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -1,17 +1,41 @@
 import { useState } from "react";
 import { createContext } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 export const AdminContext = createContext();
 
 const AdminContextProvider = (props) => {
-  const [adminToken, setAdminToken] = useState(localStorage.getItem('adminToken')?localStorage.getItem('adminToken'):"");
+  const [adminToken, setAdminToken] = useState(
+    localStorage.getItem("adminToken") ? localStorage.getItem("adminToken") : ""
+  );
+  const [doctors, setDoctors] = useState([]);
 
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+  const getAllDoctors = async () => {
+    try {
+      const { data } = await axios.post(
+        backendUrl + "/api/admin/all-doctors",
+        {},
+        { headers: { adminToken } }
+      );
+      if (data.success) {
+        setDoctors(data.doctors);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
 
   const value = {
     adminToken,
     setAdminToken,
     backendUrl,
+    doctors,
+    getAllDoctors,
   };
   return (
     <AdminContext.Provider value={value}>
