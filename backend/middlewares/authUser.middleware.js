3ecaf1717cc5e16aad21021d28a54c3bd17c8581
Abhinav1730 <abhinav.saxena1730@gmail.com>
2025-04-06 16:19:14 +0530

    Creating a backend for getting the user profile to display on the UI.

---
+++ backend/middlewares/authUser.middleware.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+
+//User authentication middleware
+const authUser = async (req, res, next) => {
+  try {
+    const { token } = req.headers;
+    if (!token) {
+      return res.json({
+        success: false,
+        message: "Not Authorised , Login Again",
+      });
+    }
+    const decodedtoken = jwt.verify(token, process.env.JWT_SECRET);
+    req.body.id = decodedtoken.id;
+    next();
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export default authUser;
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { loginUser, registerUser } from "../controllers/user.controller.js";
+import {
+  getProfile,
+  loginUser,
+  registerUser,
+} from "../controllers/user.controller.js";
+import authUser from "../middlewares/authUser.middleware.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
+userRouter.get("/get-profile", authUser, getProfile);
 
 export default userRouter;
