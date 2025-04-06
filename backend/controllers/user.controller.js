84fc06f31776f6222f70fd9f84a8145ecf0e1980
Abhinav1730 <abhinav.saxena1730@gmail.com>
2025-04-06 19:40:30 +0530

    Minor Bug Fixes

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -68,8 +68,8 @@ const loginUser = async (req, res) => {
 //API to get user profile data
 const getProfile = async (req, res) => {
   try {
-    const { userId } = req.body;
-    const userData = await userModel.findById(userId).select("-password");
+    const { id } = req.user.id;
+    const userData = await userModel.findById(id).select("-password");
     res.json({ success: true, userData });
   } catch (error) {
     console.log(error);
